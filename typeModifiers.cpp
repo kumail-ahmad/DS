a650d5ff21607773a1c2db8ca98a59e36752a837
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-04 10:07:17 +0530

    binary conversions & bitwise

---
+++ typeModifiers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    unsigned int a = -12;
+    // cout << sizeof(int) << endl;
+    // cout << sizeof(long int) << endl;
+    // cout << sizeof(short int) << endl;
+    // cout << sizeof(long long int) << endl;
+    cout << a << endl;
+    return 0;
+}
