8875fa7109bd174b4fb595e464553a0faa2bf2f0
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-05 20:27:46 +0530

    vectors DMA

--- bitwise.cpp
+++ bitwise.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <iostream>
 using namespace std;
 
@@ -8,15 +8,5 @@ int main()
     cout << (a ^ b) << endl;
     // cout << (n >> 1) << endl;
     return 0;
-=======
-#include <iostream>
-using namespace std;
 
-int main()
-{
-    int a = 6, b = 10, n = 8;
-    cout << (a ^ b) << endl;
-    // cout << (n >> 1) << endl;
-    return 0;
->>>>>>> ef9804f3ad918f6f47b363d97e147fac314ce8c6
 }
