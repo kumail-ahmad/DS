7785e958d8d45d58876f469453fccc2edf9c12d2
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-08 22:05:17 +0530

    Fibonacci recursion Qs

---
+++ fibonacciRecursive.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int fibno(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return (n - 1) + (n - 2);
+}
+int main()
+{
+    int n = 5;
+    cout << fibno(n);
+    return 0;
+};
