ed1ca62cb07dce7679f0731bf9fa5263c3b23b21
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-19 15:48:06 +0530

    Product of Array - Except self-Optimal Approach SC

--- Arrays/productOfArraysExceptSelf-Optimal.cpp
+++ Arrays/productOfArraysExceptSelf-Optimal.cpp
@@ -7,23 +7,18 @@ int main()
     int n = nums.size();
 
     vector<int> ans(n, 1);
-    vector<int> prefix(n, 1);
-    vector<int> suffix(n, 1);
 
+    int suffix = 1;
     // prefix
     for (int i = 1; i < n; i++)
     {
-        prefix[i] = prefix[i - 1] * nums[i - 1];
+        ans[i] = ans[i - 1] * nums[i - 1];
     }
     // suffix
     for (int i = n - 2; i >= 0; i--)
     {
-        suffix[i] = suffix[i + 1] * nums[i + 1];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        ans[i] = prefix[i] * suffix[i];
+        suffix *= nums[i + 1];
+        ans[i] = ans[i] * suffix;
     }
 
     // print
