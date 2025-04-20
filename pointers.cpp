be50f6add9fa86a7cc91fabdb15d53aeda2da475
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-20 22:18:20 +0530

    Double derefrencing opewrator

--- pointers.cpp
+++ pointers.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 int main()
 {
-    int a;
+    int a = 10;
     int *ptr = &a;
     int **ptr2 = &ptr;
 
     cout << "The address of a is " << ptr << "\n";
+
+    cout << "The value of a is " << *(&a) << "\n"; // using dereference operator to get the value of a
+
     cout << "The address of p is " << &ptr << "\n";
-    cout << ptr2;
+
+    cout << **(ptr2); // double derefrencing operator - to get value of a 
     return 0;
 }
