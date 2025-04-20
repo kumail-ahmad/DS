93192cdb722c62d880348aa6ed7c6aef807e9aa6
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-20 22:07:15 +0530

    pointer to a pointer

--- pointers.cpp
+++ pointers.cpp
@@ -4,8 +4,10 @@ int main()
 {
     int a;
     int *ptr = &a;
+    int **ptr2 = &ptr;
 
     cout << "The address of a is " << ptr << "\n";
-    cout << "The address of p is " << &ptr;
+    cout << "The address of p is " << &ptr << "\n";
+    cout << ptr2;
     return 0;
 }
