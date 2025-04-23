f8952ff4cb869f47c004b78f83cc59f28f7316c8
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-23 21:53:23 +0530

    Pointer arithmetic - subtraction of bytes"

---
+++ Arrays/pointerBlocks.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int a = 10;
+    int *ptr1;
+    int *ptr2 = ptr1 + 2;
+// subtarction
+    cout << ptr2 - ptr1 << endl;
+    return 0;
+}
