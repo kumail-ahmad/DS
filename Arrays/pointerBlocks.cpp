77cfb3ad08b970798bc3ffe42ccbe577c42d4207
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-23 21:56:55 +0530

    Pointer arithmetic - comparison

--- Arrays/pointerBlocks.cpp
+++ Arrays/pointerBlocks.cpp
@@ -5,8 +5,15 @@ int main()
     int arr[] = {1, 2, 3, 4, 5};
     int a = 10;
     int *ptr1;
+    int *ptr3 = ptr1;
+
     int *ptr2 = ptr1 + 2;
-// subtarction
+    // subtarction
     cout << ptr2 - ptr1 << endl;
+
+    // comparison
+    cout << (ptr1 == ptr3) << endl;
+    cout << (ptr1 < ptr3) << endl;
+    cout << (ptr1 > ptr3) << endl;
     return 0;
 }
