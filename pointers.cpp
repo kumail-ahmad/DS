7cb18a2f25437c53607545fb55088f208dacac81
Kumail Ahmad <kumailahmad217@gmail.com>
2025-04-23 17:37:49 +0530

    Used Null pointer

--- pointers.cpp
+++ pointers.cpp
@@ -6,12 +6,16 @@ int main()
     int *ptr = &a;
     int **ptr2 = &ptr;
 
+    int *nulptr = NULL;
+
     cout << "The address of a is " << ptr << "\n";
 
     cout << "The value of a is " << *(&a) << "\n"; // using dereference operator to get the value of a
 
     cout << "The address of p is " << &ptr << "\n";
 
-    cout << **(ptr2); // double derefrencing operator - to get value of a 
+    cout << **(ptr2) << endl; // double derefrencing operator - to get value of a
+
+    cout << nulptr;
     return 0;
 }
