e92a18e6e6f2b37159c52b2cedcbe3944f57ace0
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-02 22:45:05 +0530

    used vector

--- Arrays/rotatedSortedArray.cpp
+++ Arrays/rotatedSortedArray.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 int main()
 {
-    vector <int> arr  = {3, 4, 5, 6, 7, 0, 1, 2};
-    cout<<arr.size()<<endl;
+    vector<int> arr = {3, 4, 5, 6, 7, 0, 1, 2};
+    cout << "The size of array is :" << arr.size() << endl;
     return 0;
 }
