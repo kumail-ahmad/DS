fb360a4dc4f9a1b8abcfa7c6b41d22dadd75c641
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-03 22:08:48 +0530

    rotatedArray - debug Print

--- Arrays/rotatedSortedArray.cpp
+++ Arrays/rotatedSortedArray.cpp
@@ -5,5 +5,9 @@ int main()
 {
     vector<int> arr = {3, 4, 5, 6, 7, 0, 1, 2};
     cout << "The size of array is :" << arr.size() << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    };
     return 0;
 }
