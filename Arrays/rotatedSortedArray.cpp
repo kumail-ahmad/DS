80fa12505b63707ea490494a000d068ec977d7ea
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-12 22:22:16 +0530

    rotated sorted array

--- Arrays/rotatedSortedArray.cpp
+++ Arrays/rotatedSortedArray.cpp
@@ -4,12 +4,22 @@ using namespace std;
 int main()
 {
     vector<int> arr = {3, 4, 5, 6, 7, 0, 1, 2};
-    cout << "The size of array is :" << arr.size() << endl;
-    cout << "The printed array is :" << endl;
-    for (int i = 0; i < arr.size(); i++)
+    int start = arr[0];
+    int target = 0;
+    int end = arr.size() - 1;
+    while (start <= end)
     {
-        cout << arr[i] << endl;
-        ;
-    };
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target)
+        {
+            return mid;
+        };
+        if (arr[start] <= arr[mid])
+        { // left
+        }
+        else
+        { // right
+        }
+    }
     return 0;
 }
