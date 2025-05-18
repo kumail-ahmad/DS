0d91d9681f84edac6c7ff8f593a48e1932b628a3
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-18 22:17:06 +0530

    practice -value change

--- exam.c
+++ exam.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int x = 3, y = 3, z = 1;
+    int x = 3, y = 3, z = 2;
     z += x < y ? x++ : y++;
     printf("%d\t%d\t%d\n", x, y, z);
     printf("%d\t%d\t%d\n", x, y, z);
