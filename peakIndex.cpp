a98f1d49a96216985a138bb8a31487af8f811e82
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-20 23:04:57 +0530

    peak index mountain - declared variables

--- peakIndex.cpp
+++ peakIndex.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main() {
-  
-return 0;
+int main()
+{
+    vector<int> arr = {0, 3, 8, 9, 5, 2};
+    int start = 0;
+    int target = 9;
+    int end = arr.size() - 1;
+    while (arr[start] <= arr[end])
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target)
+        {
+            /* code */
+        }
+    }
+
+    return 0;
 }
