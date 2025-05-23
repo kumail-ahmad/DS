62e833b9b637ef10e2d4227b208ba3d62acf820b
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-23 22:56:06 +0530

    single Element - determined

--- Arrays/singleElementInASortedArray.cpp
+++ Arrays/singleElementInASortedArray.cpp
@@ -5,11 +5,22 @@ int main()
 {
     vector<int> arr = {1, 1, 2, 3, 3, 4, 4, 8, 8};
     int start = 0, end = arr.size();
+    int mid = start + (end - start) / 2;
     for (int i = 0; i < end; i++)
     {
-        if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
+        if (mid % 2 == 0)
+        { // even case
+            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
+            {
+                return mid;
+            }
+            else if (mid % 2 != 0)
+            {
+            }
+        }
+        else
         {
-            cout << "The number is at index :" << i;
+            // odd case
         }
     }
     return 0;
