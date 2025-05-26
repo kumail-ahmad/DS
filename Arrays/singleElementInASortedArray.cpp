4c15ba97be82d3c8da02a4c1e2e1af9677e30c9f
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-26 20:56:51 +0530

    single Element - handled Edge cases

--- Arrays/singleElementInASortedArray.cpp
+++ Arrays/singleElementInASortedArray.cpp
@@ -4,38 +4,57 @@ using namespace std;
 int main()
 {
     vector<int> arr = {1, 1, 2, 3, 3, 4, 4, 8, 8};
-    int start = 0, end = arr.size();
-    int mid = start + (end - start) / 2;
+    int n = arr.size();
+    int start = 0, end = n - 1;
+    int mid;
     while (start <= end)
     {
+        mid = start + (end - start) / 2;
+
+        // edge case
+        if (mid == 0 && arr[0] != arr[1])
+
+        {
+            cout << arr[mid] << endl;
+            return 0;
+        }
+        if (mid == n - 1 && arr[n - 1] != arr[n - 2])
+
+        {
+            cout << arr[mid] << endl;
+            return 0;
+        }
+        if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
+        {
+            cout << arr[mid] << endl;
+            return 0;
+        };
+
         if (mid % 2 == 0)
         { // even case
-            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
+
+            if (arr[mid - 1] == arr[mid])
             {
-                return mid;
-            }
-            else if (arr[mid - 1] == arr[mid])
-            {
-                end = mid - 1;
+                end = mid - 1; // left
             }
             else
             {
-                start = mid + 1;
+                start = mid + 1; // right
             }
         }
         else
         {
             // odd case
-            if (arr[mid] == arr[mid])
+            if (arr[mid - 1] == arr[mid])
             {
-                start = mid + 1;
+                start = mid + 1; // right
             }
             else
             {
-                end = mid - 1;
+                end = mid - 1; // left
             }
         }
     }
-    cout << "element found index:" << mid << endl;
+    cout << "element found index:" << arr[mid] << endl;
     return -1;
 }
