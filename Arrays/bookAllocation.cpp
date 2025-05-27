1f6a8115c1b475e3a17178ee7a11a4e1aa238a01
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-27 20:21:31 +0530

    Book allocation - allocated books function

--- Arrays/bookAllocation.cpp
+++ Arrays/bookAllocation.cpp
@@ -2,8 +2,31 @@
 #include <vector>
 using namespace std;
 
+bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages)
+{
+    int student = 1, pages = 0;
+
+    for (int i = 01; i < n; i++)
+    {
+        if (pages + arr[i] <= maxAllowedPages)
+        {
+            pages = pages + arr[i];
+        }
+        else
+        {
+            student++;
+            pages = arr[i];
+        }
+    }
+}
+
 int allocateBooks(vector<int> &arr, int n, int m)
 {
+    if (m > n)
+    {
+        return -1; // Not enough books for students
+    }
+
     int sum = 0;
     for (int i = 0; i < n; i++)
     {
@@ -12,12 +35,23 @@ int allocateBooks(vector<int> &arr, int n, int m)
     int start = 0;
     int end = sum, ans = -1;
 
-    while (start<=end)
+    while (start <= end)
     {
-        int mid=start +(end-start)/2;
-        
+        int mid = start + (end - start) / 2;
+
+        if (isValid(arr, n, m, mid))
+        {
+            // valid -left
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+        {
+            // invalid - right
+            start = mid + 1;
+        }
     }
-    
+    return ans;
 }
 
 int main()
