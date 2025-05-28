0b0250f8b1c2d6e69808e594cac65ba97b750b21
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-28 21:03:40 +0530

    Selection sort - logic done

--- Arrays/sorting/selectionSort.cpp
+++ Arrays/sorting/selectionSort.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
-int main() {
-  
-return 0;
+void SelectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int smallestIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[smallestIndex])
+            {
+                smallestIndex = j;
+            }
+        }
+        swap(arr[i], arr[smallestIndex]);
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    SelectionSort(arr, n);
+    
+    return 0;
 }
