6113194008f4ba021a132cf9570a59af8b831a87
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-29 20:52:17 +0530

    Insertion Sort - logic for sorting

--- Arrays/sorting/insertionSort.cpp
+++ Arrays/sorting/insertionSort.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
+
+void insertionSort(int arr[], int n) // O(n^2) time complexity, O(1) space complexity
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int prev = i - 1;
+
+        while (prev >= 0 && arr[prev] > current)
+        {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = current;
+    }
+}
+
 int main()
 {
+    int arr[] = {4, 1, 5, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
 
     return 0;
 }
