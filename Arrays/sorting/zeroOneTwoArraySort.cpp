af52b8cfcd5a6403819970de372b4a378f7f33ef
Kumail Ahmad <kumailahmad217@gmail.com>
2025-05-31 22:18:48 +0530

    0,1,2's array Sort - optimal approach

--- Arrays/sorting/zeroOneTwoArraySort.cpp
+++ Arrays/sorting/zeroOneTwoArraySort.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-int main() {
-  
-return 0;
+int main()
+{
+    int zero = 0, one = 0, two = 0;
+    int arr[] = {2, 0, 2, 1, 1, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n - 1; i++) // O(n) time complexity, O(1) space complexity
+    {
+        if (arr[i] == 0)
+            zero++;
+
+        else if (arr[i] == 1)
+            one++;
+
+        else if (arr[i] == 2)
+            two++;
+    }
+ 
+
+    return 0;
 }
