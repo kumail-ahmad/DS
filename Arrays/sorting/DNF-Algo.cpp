f2af05eb8b2f6fef3a836212789a0e8446b0b7a6
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-01 18:41:47 +0530

    0,1,2's DNF algorithm  - wrote details

--- Arrays/sorting/DNF-Algo.cpp
+++ Arrays/sorting/DNF-Algo.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 int main()
-{
+{// Dutch national flag problem O(n) time complexity, O(1) space complexity
     vector<int> arr = {2, 0, 2, 1, 1, 0};
     int n = arr.size();
     int low = 0, mid = 0, high = n - 1;
