97651c9970ed72d1810ebbb6d73941f1f5209b2f
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-04 22:17:57 +0530

    Merge sorted arrays  - wrote logic

--- Arrays/sorting/mergeSortedArray.cpp
+++ Arrays/sorting/mergeSortedArray.cpp
@@ -4,7 +4,25 @@
 using namespace std;
 int main()
 {
-    vector<int> arr1 = {1, 2, 3, 0, 0, 0};
-    vector<int> arr2 = {2, 5, 6};
+    vector<int> A = {1, 2, 3, 0, 0, 0};
+    vector<int> B = {2, 5, 6};
+    int m = A.size() - 1;
+    int n = B.size() - 1;
+    int i = m - 1,
+        j = n - 1, index = 5; // i and j are the last indices of A and B respectively, index is the last index of A
+    while (i >= 0 && j >= 0)
+    {
+        if (A[i] > B[j])
+        {
+            A[index] = A[i];
+            i--;
+        }
+        else
+        {
+            A[index] = B[j];
+            j--;
+            index--;
+        }
+    }
     return 0;
 }
