4591249d3e47cb292c56f80625d848db4f45d4ea
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-05 10:20:03 +0530

    Merge sorted arrays  - fixed indexing

--- Arrays/sorting/mergeSortedArray.cpp
+++ Arrays/sorting/mergeSortedArray.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main()
 {
-    vector<int> A = {1, 2, 3, 0, 0, 0};
+    vector<int> A = {1, 2, 3, 0, 0, 0}; // TC:O(m + n), SC:O(1)
     vector<int> B = {2, 5, 6};
     int m = 3;
     int n = 3;
     int i = m - 1,
-        j = n - 1, index = 5; // i and j are the last indices of A and B respectively, index is the last index of A
+        j = n - 1, index = m + n - 1; // i and j are the last indices of A and B respectively, index is the last index of A
     while (i >= 0 && j >= 0)
     {
         if (A[i] >= B[j])
