688413f794787e77a2eaf83e45d281909cf626a9
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-11 21:18:38 +0530

    Hollow triangle

---
+++ Hollow-Triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            if (j == 1 || j == i || i == 5)
+
+                printf("*");
+
+            else
+
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
