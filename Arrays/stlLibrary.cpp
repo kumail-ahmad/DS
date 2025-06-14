b18ea37288ea5a9a0f7ea3c6c7ed7e869647bb97
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-14 22:28:53 +0530

    Vectors - push back function

--- Arrays/stlLibrary.cpp
+++ Arrays/stlLibrary.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main() {
-  
-return 0;
+int main()
+{
+    // vector
+    vector<int> vec = {1, 3, 5, 6, 7, 8, 9, 10};
+
+    vec.push_back(11);
+    cout << vec.size() << endl;
+    return 0;
 }
