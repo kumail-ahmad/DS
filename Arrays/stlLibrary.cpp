4649359636727d162db90fc3d157703000272df5
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-15 21:32:43 +0530

    Vectors - printing front and back element

--- Arrays/stlLibrary.cpp
+++ Arrays/stlLibrary.cpp
@@ -14,13 +14,16 @@ int main()
     // cout << vec.size() << endl;
     // cout << vec.capacity() << endl;
     vec.pop_back();
-
+    vec.emplace_back();
     for (int i : vec)
     {
         cout << i << " ";
     }
 
     cout << endl;
-    cout << "value at index 2 " << vec[2] << " or " << vec.at(2) << endl;
+    // cout << "value at index 2 " << vec[2] << " or " << vec.at(2) << endl;
+
+    cout << vec.front() << endl; // first element
+    cout << vec.back() << endl;  // last element
     return 0;
 }
