1607f6556ab8a18dcbf5718ab1a51879173ec128
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-17 22:55:48 +0530

    Vectors - erase function - range erase

--- Arrays/stlLibrary.cpp
+++ Arrays/stlLibrary.cpp
@@ -5,7 +5,7 @@ int main()
 {
     // vector
     vector<int> vec;
-    vector<int> dynamic(10, -1);
+    vector<int> dynamic(5, 2);
 
     vector<int> dynoCopy(dynamic);
     vec.push_back(11);
@@ -15,20 +15,20 @@ int main()
 
     // cout << vec.size() << endl;
     // cout << vec.capacity() << endl;
-    vec.pop_back();
-    vec.emplace_back();
+    //  vec.pop_back();
+    // vec.emplace_back();
 
     cout << endl;
     // cout << "value at index 2 " << vec[2] << " or " << vec.at(2) << endl;
 
-    cout << vec.front() << endl; // first element
-    cout << vec.back() << endl;  // last element
+    //  cout << vec.front() << endl; // first element
+    // cout << vec.back() << endl;  // last element
 
     // erase function
-    vec.erase(vec.begin() + 2);
+    vec.erase(vec.begin() + 1, vec.begin() + 3);
 
     // print the vector
-    for (int i : dynoCopy)
+    for (int i : vec)
     {
         cout << i << " ";
     }
