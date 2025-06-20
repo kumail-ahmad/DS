e8ab3346c677153b8ec9158af560475650399576
Kumail Ahmad <kumailahmad217@gmail.com>
2025-06-20 12:00:00 +0530

    file Handling - reading a file

--- fileHandling.c
+++ fileHandling.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    FILE *f = fopen("test.txt", "w");
-    if (f == NULL) {
+int main()
+{
+    FILE *f = fopen("test.txt", "r");
+
+    char line[100];
+    if (f == NULL)
+    {
         printf("Error opening file!\n");
         return 1;
     }
     fprintf(f, "Hello, Kumail!\n");
+    while (fgets(line, sizeof(line), f))
+    {
+        printf("%s", line);
+    }
     fclose(f);
     return 0;
 }
