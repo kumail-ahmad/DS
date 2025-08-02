b5813bdbdd16192043bdb1dfc692e8f4b46a9148
Kumail Ahmad <kumailahmad217@gmail.com>
2025-08-02 12:00:00 +0530

    Strings - Dynamic changes

---
+++ strings/stringFunc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str1 = "kumail";
+    string str2 = "Ahmad";
+
+    string str3 = str1 + str2;
+    cout << " concatenated string:" << str3 << endl;
+    return 0;
+}
