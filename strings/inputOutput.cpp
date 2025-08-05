a240856b58e474addb1c9a7721ef9f65a339dead
Kumail Ahmad <kumailahmad217@gmail.com>
2025-08-05 20:58:21 +0530

    String in Cpp

---
+++ strings/inputOutput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[100];
+    cout << "enter a string:" << endl;
+    cin.getline(str, 100, '$');
+    // cout << "your output is : " << str;
+
+    for (char ch : str)
+    {
+        cout << ch << " ";
+    }
+
+    return 0;
+}
--- strings/strings.cpp
+++ strings/strings.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
-int main() {
-  
-return 0;
+int main()
+{
+    char str[] = {"hello world"};
+    cout << "length is :"
+         << strlen(str) << endl
+         << str << endl;
+
+    return 0;
 }
