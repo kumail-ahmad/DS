1f3283cfd30723184ee6ea2e16643fdf41797fa9
Kumail Ahmad <kumailahmad217@gmail.com>
2025-08-05 21:06:08 +0530

    Strings in Cpp

--- strings/inputOutput.cpp
+++ strings/inputOutput.cpp
@@ -4,7 +4,7 @@ int main()
 {
     char str[100];
     cout << "enter a string:" << endl;
-    cin.getline(str, 100, '$');
+    cin.getline(str, 100);
     // cout << "your output is : " << str;
 
     for (char ch : str)
