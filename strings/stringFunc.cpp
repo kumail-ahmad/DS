0b15c9ad2636d213f6994f9aa6578691fad5f010
Kumail Ahmad <kumailahmad217@gmail.com>
2025-08-06 11:23:36 +0530

    Strings in Cpp - Input in  String

--- strings/stringFunc.cpp
+++ strings/stringFunc.cpp
@@ -6,11 +6,18 @@ int main()
     string str2 = "Ahmad";
 
     string str3 = str1 + str2;
+    string str4;
+
     // concatenation
     cout << " concatenated string: " << str3 << endl;
     // compare
     cout << "are the string same: " << (str1 == str2) << endl;
     // length
     cout << " The length of the string is: " << str1.length() << endl;
+
+    // input a string
+    cout << "Enter a string: ";
+    cin >> str4; // stored only one word
+    cout << "The string you entered is: " << str4 << endl;
     return 0;
 }
