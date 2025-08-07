6a7ccf97e7a97ca1ef5460f07a9400f895aacdeb
Kumail Ahmad <kumailahmad217@gmail.com>
2025-08-07 11:03:59 +0530

    String - Using Built in func

--- strings/StringReverse.cpp
+++ strings/StringReverse.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 int main()
 { // string array
+   
     string str1 = "kumail";
     int start = 0, end = str1.length() - 1;
     while (start <= end)
@@ -11,5 +14,10 @@ int main()
         end--;
     }
     cout << "Reversed string: " << str1 << endl;
+
+    
+    reverse(str1.begin(), str1.end());
+    cout << "Again Reversed string: " << str1 << endl;
+
     return 0;
 }
