5b26e0c8dc354a74357bb136b368556a90841430
Kumail Ahmad <kumailahmad217@gmail.com>
2025-08-19 21:52:56 +0530

    Linked List - ()

---
+++ Linked Lists/code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+public:
+    Node *head;
+    Node *tail;
+    List()
+    {
+    }
+};
+int main()
+{
+
+    return 0;
+}
