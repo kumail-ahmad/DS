6a780e43d0285de292f2aeeece7de110365987d1
Kumail Ahmad <kumailahmad217@gmail.com>
2025-08-20 22:26:09 +0530

    Linked List - push Front

--- Linked Lists/code.cpp
+++ Linked Lists/code.cpp
@@ -20,10 +20,22 @@ public:
     Node *tail;
     List()
     {
+        head = tail = NULL;
+    }
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);//dynamic
+       // Node newNode(val); // static
+       if (head==NULL)
+       {
+        head=tail=newNode;
+        return;
+       }
+       
     }
 };
 int main()
 {
-
+List ll;
     return 0;
 }
