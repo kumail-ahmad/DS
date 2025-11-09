63fe237e3441d5d6ff150abd78e9c150ba733478
Kumail Ahmad <kumailahmad217@gmail.com>
2025-11-09 19:34:58 +0530

    Tree - level traversal- endl

--- binaryTree/levelOrder.cpp
+++ binaryTree/levelOrder.cpp
@@ -36,12 +36,24 @@ void levelOrder(Node *root)
 {
     queue<Node *> q;
     q.push(root);
+    q.push(NULL);
 
     while (q.size() > 0)
     {
         Node *curr = q.front();
         q.pop();
-        cout << curr->data<<" ";
+        if (curr == NULL)
+        {
+            if (!q.empty())
+            {
+                cout << endl;
+                q.push(NULL);
+                continue;
+            }
+            else
+                break;
+        }
+        cout << curr->data << " ";
         if (curr->left != NULL)
         {
             q.push(curr->left);
@@ -56,7 +68,7 @@ void levelOrder(Node *root)
 
 int main()
 {
-    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, 5, -1, -1};
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node *root = buildTree(preorder);
     levelOrder(root);
     return 0;
