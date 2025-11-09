9533ec568f08a4beb85669e427e5fd0e7e86356d
Kumail Ahmad <kumailahmad217@gmail.com>
2025-11-09 15:52:30 +0530

    Tree - Preorder traversal

---
+++ binaryTree/preorder.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+static int idx = -1;
+Node *buildTree(vector<int> &preOrder)
+{
+    idx++;
+    if (idx >= preOrder.size() || preOrder[idx] == -1)
+
+        return NULL;
+    Node *root = new Node(preOrder[idx]);
+    root->left = buildTree(preOrder);
+    root->right = buildTree(preOrder);
+    return root;
+}
+
+void preOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int main()
+{
+
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, 5, -1, -1};
+    Node *root = buildTree(preorder);
+    preOrder(root);
+    // cout << root->data << endl;
+    return 0;
+}
